dd6a2fe412132ca711f362c012f5c56be112958a
Artsyom Pereverzev <artsiom.peraverzeu@cogniteq.com>
2019-03-21 12:41:56 +0300

    Imported React Router Dom.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React, { Component } from "react";
+import React, { Fragment } from 'react'
+import { Router, Switch, Route } from 'react-router-dom'
+import history from './history'
 
-import './styles/App.css';
+import {
+  PrivateRoute
+} from './components'
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>My React App!</h1>
-            </div>
-        );
-    }
+import {
+  LoginPage,
+  WelcomePage,
+  Dashboard,
+  ToDo
+} from './pages'
+
+function App () {
+  return (
+    <Fragment>
+      <Router history={history}>
+        <Switch>
+          <PrivateRoute exact path='/' component={WelcomePage} />
+          <PrivateRoute path='/dashboard' component={Dashboard} />
+          <PrivateRoute path='/todo' component={ToDo} />
+          <Route exact path='/login' component={LoginPage} />
+
+          <Route path='*' render={() => <div>404</div>} />
+        </Switch>
+      </Router>
+    </Fragment>
+  )
 }
 
-export default App;
+export default App

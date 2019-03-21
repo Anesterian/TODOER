823d91dd77c803af8e7df008bdb8f7c4cfe16358
Artsyom Pereverzev <artsiom.peraverzeu@cogniteq.com>
2019-03-21 11:25:14 +0300

    Created project Structure.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,29 @@ import 'typeface-ubuntu'
 import { Router, Switch, Route } from 'react-router-dom'
 
 import {
+  PrivateRoute
 } from './components'
 
 import {
+  LoginPage,
+  WelcomePage,
+  Dashboard,
+  ToDo
 } from './views'
 
 function App () {
   return (
     <Fragment>
+      <Router>
+        <Switch>
+          <PrivateRoute exact path='/' component={WelcomePage} />
+          <PrivateRoute path='/dashboard' component={Dashboard} />
+          <PrivateRoute path='/todo' component={ToDo} />
+          <Route exact path='/login' component={LoginPage} />
+
+          <Route path='*' render={() => <div>404</div>} />
+        </Switch>
+      </Router>
     </Fragment>
   )
 }
---
+++ src/views/index.js
@@ -0,0 +1,10 @@
+import LoginPage from './LoginPage'
+import Dashboard from './Dashboard'
+import ToDo from './ToDo'
+import WelcomePage from './WelcomePage'
+export {
+  LoginPage,
+  WelcomePage,
+  Dashboard,
+  ToDo
+}

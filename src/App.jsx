2d511117d8b332f6c2b4f98b8ac72acf095a2760
Artsyom Pereverzev <artsiom.peraverzeu@cogniteq.com>
2019-03-21 10:59:30 +0300

    Configured Webpack for React development.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react'
+import 'typeface-ubuntu'
+import { Router, Switch, Route } from 'react-router-dom'
+
+import {
+} from './components'
+
+import {
+} from './views'
+
+function App () {
+  return (
+    <Fragment>
+    </Fragment>
+  )
+}
+
+export default App

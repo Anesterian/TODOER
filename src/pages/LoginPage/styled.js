03bdacda4a4f5e4466194161ea86aa1919eee974
Artsyom Pereverzev <artsiom.peraverzeu@cogniteq.com>
2019-03-26 16:42:03 +0300

    Login done(not perfect, but done).

--- src/pages/LoginPage/styled.js
+++ src/pages/LoginPage/styled.js
@@ -1,11 +1,13 @@
 import styled from 'styled-components'
 
 const Login = styled.div`
-  margin: 0 -1rem;
+  margin: 0 45rem;
   display: flex;
   flex-direction: column;
 `
 Login.Text = styled.label`
+text-align: center;
+max-width: 20rem;
 `
 Login.Field = styled.input`
 max-width: 20rem;

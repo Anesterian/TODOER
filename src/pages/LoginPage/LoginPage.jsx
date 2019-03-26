e291d8c4406eeb68eb2e71c3b9914c679e0070af
Artsyom Pereverzev <artsiom.peraverzeu@cogniteq.com>
2019-03-26 15:43:50 +0300

    Getting user input login and password.

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -2,14 +2,23 @@ import React, { useState } from 'react'
 import {Login} from './styled'
 
 const LoginPage = () => {
-  const [username, setUsername] = useState([])
-  const [password, setPassword] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [value, setValue] = useState('')
+  const validateForm = (name, pass) =>  {
+    if ((name.length > 0) && (pass.length > 0)) {
+      return true
+    }
+  }
+  const handleClick = (event) => {
+    setUsername(event.target.username)
+  }
 return(
   <Login>
     <Login.Text>Login</Login.Text>
-    <Login.Field placeholder="Enter your username" onChange={(event, newValue) => setUsername(newValue)}/>
-    <Login.Field placeholder="Enter your password" onChange={(event, newValue) => setPassword(newValue)}/>
-    <Login.Field type="submit" label="Submit" onClick={(event) => this.handleClick(event)}/>
+    <Login.Field placeholder="Enter your username" onChange={event => setUsername(event.target.value)}/>
+    <Login.Field placeholder="Enter your password" onChange={event => setPassword(event.target.value)}/>
+    <Login.Field type="submit" label="Submit" value="Submit" disabled={!(username.length && password.length)} onClick={() => {console.log(username, password)}}/>
   </Login>
   )
 }
--- src/pages/LoginPage/styled.js
+++ src/pages/LoginPage/styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-const Login = styled.form`
+const Login = styled.div`
   margin: 0 -1rem;
   display: flex;
   flex-direction: column;
@@ -9,6 +9,7 @@ Login.Text = styled.label`
 `
 Login.Field = styled.input`
 max-width: 20rem;
+color: black;
 `
 export {
   Login

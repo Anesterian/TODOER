d07e14fdc4cc4e3f5dfd28f4fc0ee95359684664
Artsyom Pereverzev <artsiom.peraverzeu@cogniteq.com>
2019-03-26 12:19:24 +0300

    Small fix of submit button.

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -9,7 +9,7 @@ return(
     <Login.Text>Login</Login.Text>
     <Login.Field placeholder="Enter your username" onChange={(event, newValue) => setUsername(newValue)}/>
     <Login.Field placeholder="Enter your password" onChange={(event, newValue) => setPassword(newValue)}/>
-    <Login.Field type="submit"/>
+    <Login.Field type="submit" label="Submit" onClick={(event) => this.handleClick(event)}/>
   </Login>
 )
 }

6865ea43a0efacc38c793e9bc3fe0abd9c67b3f7
Artsyom Pereverzev <artsiom.peraverzeu@cogniteq.com>
2019-03-27 17:04:49 +0300

    Small fix.

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -6,37 +6,37 @@ const tradeThings = [
   {
     id: 1,
     title: 'Thing 1',
-    description: 'Description for Thing1',
+    description: 'Description for Thing 1',
     price: 30
   },
   {
     id: 2,
-    title: 'Thing2',
-    description: 'Description for Thing2',
+    title: 'Thing 2',
+    description: 'Description for Thing 2',
     price: 45
   },
   {
     id: 3,
-    title: 'Thing3',
-    description: 'Description for Thing3',
+    title: 'Thing 3',
+    description: 'Description for Thing 3',
     price: 20
   },
   {
     id: 4,
-    title: 'Thing4',
-    description: 'Description for Thing4',
+    title: 'Thing 4',
+    description: 'Description for Thing 4',
     price: 130
   },
   {
     id: 5,
-    title: 'Thing5',
-    description: 'Description for Thing5',
+    title: 'Thing 5',
+    description: 'Description for Thing 5',
     price: 5
   },
   {
     id: 6,
-    title: 'Thing6',
-    description: 'Description for Thing6',
+    title: 'Thing 6',
+    description: 'Description for Thing 6',
     price: 500
   },
 ]

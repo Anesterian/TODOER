dc4aea1b6383b5f3922a179adbff455d58cb6d87
Artsyom Pereverzev <artsiom.peraverzeu@cogniteq.com>
2019-04-01 14:34:52 +0300

    import

--- src/components/Header/internal/ProfileSetting/ProfileSetting.jsx
+++ src/components/Header/internal/ProfileSetting/ProfileSetting.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { Profile } from './styled'
 import history from '../../../../history'
-import { store } from '../../../../store'
+import Shop from '../../../../pages'
 
 
 
